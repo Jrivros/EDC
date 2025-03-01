386fba38075cd40340279e76b01a0afa53e24aed
VanessaMendez17 <vane.mendez.1794@gmail.com>
2025-03-01 14:35:05 -0600

    Ejercicio 1 primera parte Diana.py
    Resolvì la primera parte

--- Diana.py
+++ Diana.py
@@ -1,1 +1,4 @@
-print("Hola")
+# Realizar el codigo para crear una lista con cinco numeros, y usando una funcion genere un diccionario con cada uno de los numeros confirmando si "es par" o "es impar"
+lista=[]
+for num in range 5:
+lista.append(int(input("ingrese un valor")))
